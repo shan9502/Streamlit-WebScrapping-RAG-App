4003563ed5ee149fb337c4253429415c5c0a0c17
shan <shanshan9502@gmail.com>
2024-06-15 18:50:45 +0530

    new chages

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import solara as sl
 import re
-from src.data_scraper import get_scrap_data
+from src.data_scraper import get_scrap_data,get_sitemap
 
 text = sl.reactive("")
 
@@ -25,20 +25,59 @@ def Page():
     sl.AppBarTitle("Chat!")
     with sl.Sidebar():
         sl.InputText("Enter the website ", value=text, continuous_update=False)
-        if text.value :
-            domine_website_url = text.value
-            sl.Markdown(f"Entered website is : {domine_website_url}")
-            sl.Markdown("Analyzing website .......")
-            is_url_validation_success = validate_website(domine_website_url)
-            if is_url_validation_success:
-                sl.Markdown("Good url")
-                response = get_scrap_data(domine_website_url)
-                if response['hasError'] == False:
-                    scrapDataFrame = response['data']
-                    scrapDataFrame.to_csv("scrap_data.csv")
-                    sl.Markdown("/Csv saved successfully./",style={"color":"green"})
+        if sl.Button(label = "Go"):
+            if text.value :
+                domine_website_url = text.value
+                sl.Markdown(f"Entered website is : {domine_website_url}")
+                sl.Markdown("Analyzing website .......")
+                is_domine_validation_success = validate_website(domine_website_url)
+                if is_domine_validation_success:
+                    sl.Markdown("Examining Website .......")
+                    domine_subpages_list = get_sitemap(domine_website_url)
+                    page_list_length = len(domine_subpages_list)
+                    if page_list_length > 1:
+                        # scrap_limit_range_list = list(range(1,page_list_length+1))
+                        if page_list_length < 10:
+                            preset_value = page_list_length
+                        elif page_list_length > 100:
+                            preset_value = 50
+
+                        sl.Markdown(f"The website have {page_list_length} sub pages.")
+                        scrap_limit = sl.SliderInt(label="How deep you want to Go..",value=preset_value,
+                                                    min=1,max=page_list_length)
+                        print("here1")
+                    elif page_list_length == 0:
+                        print("here2")
+                        domine_subpages_list = [domine_website_url]
+                        page_list_length = 1
+                        scrap_limit = 2
+                    else:
+                        print("here3")
+                        scrap_limit = 2
+                    
+                    if page_list_length != 0:
+                        is_confirm_status = False
+                        scrap_img = sl.Checkbox(label="Do you want to analyze the images also!",value=False)
+                        sl.Button(label = "Confirm", on_click = is_confirm)
+                        if is_confirm_status:
+                            print("scrap_limit:",scrap_limit)
+                            domine_subpages_list = domine_subpages_list[0:scrap_limit]
+                            sl.Markdown("Scraping process started.........")
+                            response = get_scrap_data(domine_subpages_list,scrap_img)
+                            
+                            print("Reached for get_scrap_data()")
+                            print("domine_subpages_list:",len(domine_subpages_list),"\nscrap_img:",scrap_img)
+                            # sl.Markdown("Good url")
+                            # response = get_scrap_data(domine_website_url)
+                            if response['hasError'] == False:
+                                scrapDataFrame = response['data']
+                                scrapDataFrame.to_csv("test/scrap_data.csv")
+                                sl.Markdown("/Csv saved successfully./",style={"color":"green"})
+                            else:
+                                sl.Markdown("Error Occurred!",style={"color":"red"})
                 else:
-                    sl.Markdown("Error Occurred!",style={"color":"red"})
-            else:
-                sl.Markdown("Please enter a valid website !",style={"color":"red"})
+                    sl.Markdown("Please enter a valid website !",style={"color":"red"})
+
+def is_confirm():
+    is_confirm_status = True
         
--- src/data_scraper.py
+++ src/data_scraper.py
@@ -4,16 +4,34 @@ import pandas as pd
 from usp.tree import sitemap_tree_for_homepage
 from app_logger import logger
 
-def get_pages_from_sitemap(fullDomain):
-    try:
-        listPagesRaw = []
-        tree = sitemap_tree_for_homepage(fullDomain)
-        for page in tree.all_pages():
-            listPagesRaw.append(page.url)
-        return listPagesRaw
+def get_sitemap(fullDomain):
+    rawPagesList = []
+    try:       
+        # tree = sitemap_tree_for_homepage(fullDomain)
+        # for page in tree.all_pages():
+        #     rawPagesList.append(page.url)
+        print("get_sitemap called!!!!")
+        return rawPagesList
+
     except Exception as e:
         logger.error(e)
-        return []
+        rawPagesList
+
+def get_img_urls(raw_img_list):
+    # list_img_list = []
+    try:
+        img_list = []
+        if len(raw_img_list)>0:
+            for img in raw_img_list:
+                img_list.append(img['src']) 
+            # list_img_list.append(img_list)
+            return img_list
+        else:
+            return None
+    except Exception as e:
+        logger.error(e)
+        print(e)
+        return None
     
 def text_preprocess(web_text):
     current_text = web_text.replace("\n\n","\n").replace("\n"," ")
@@ -22,39 +40,37 @@ def text_preprocess(web_text):
     # print(current_text)
     return current_text
     
-def get_scrap_data(domain_url):
+def get_scrap_data(pageUrlList,scrap_img=False):
     url_list = []
     text_list = []
     list_img_list = []
     scrap_data_frame = pd.DataFrame()
     
-    listPagesRaw = get_pages_from_sitemap(domain_url)
-    if len(listPagesRaw) == 0:
-        return {"hasError":True, "data":scrap_data_frame,"message":"Sitemap Error!"}
-    pageShortList = listPagesRaw[0:10]
-    
+    if len(pageUrlList) == 0:
+        return {"hasError":True, "data":scrap_data_frame,"message":"Sitemap Error!"}    
     try:
-        for url in pageShortList:
+        for url in pageUrlList:
             req = Request(url, headers={'User-Agent' : "Magic Browser"}) 
             page = urlopen( req )
             html = page.read().decode("utf-8")
-            soup = BeautifulSoup(html, "html.parser")
-            raw_img_list = soup.find_all("img")
+            soup = BeautifulSoup(html, "html.parser")           
             url_list.append(url)
             processed_text = text_preprocess(soup.get_text())
             text_list.append(processed_text)
-            img_list = []
-            for img in raw_img_list:
-                for img_list_check in list_img_list:
-                    if img['src'] not in img_list_check:
-                       img_list.append(img['src']) 
-            list_img_list.append(img_list)
+            if scrap_img == True:
+                #get all img urls from soup
+                raw_img_list = soup.find_all("img")
+                img_list = get_img_urls(raw_img_list)
+                list_img_list.append(img_list)
+            
         scrap_data_frame['urls'] = url_list
         scrap_data_frame['texts'] = text_list
-        scrap_data_frame['image_url_list'] = list_img_list
+        scrap_data_frame['image_url_list'] = list_img_list if list_img_list else None
+        print("Length of page url length:",len(pageUrlList))
         return {"hasError":False, "data":scrap_data_frame}
     except Exception as e:
         logger.error(e)
+        print(e)
         return {"hasError":True, "data":scrap_data_frame,"message":e}
         
     
